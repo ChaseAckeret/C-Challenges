c809fe35993b054cbea48162c840931b56d56d83
ChaseAckeret <chaseackeret@gmail.com>
2020-11-21 13:59:11 -0500

    Initial Commit

---
+++ CSharpChallenges/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CSharpChallenges")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CSharpChallenges")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("061256c5-219e-4dbb-8234-243fc74c57a5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CSharpChallenges/Questions.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharpChallenges
+{
+    [TestClass]
+    public class Questions
+    {
+        [TestMethod]
+        public void Question1()
+        {
+            string firstName;
+            string lastName;
+            int age;
+
+            firstName = "Chase";
+            lastName = "Ackeret";
+            age = 28;
+            Console.WriteLine($"My name is {firstName } {lastName}, and I am {age}.");
+        }
+
+        [TestMethod]
+        public void Question2()
+        {
+
+        }
+
+        [TestMethod]
+        public void Question3()
+        {
+
+        }
+
+        [TestMethod]
+        public void Question4()
+        {
+
+        }
+
+        [TestMethod]
+        public void Question5()
+        {
+
+        }
+
+        [TestMethod]
+        public void Question6()
+        {
+            string firstQuestion = "How are you doing today?";
+            Console.WriteLine(firstQuestion);
+            string answer = Console.ReadLine().ToLower();
+
+            switch (answer)
+            {
+                case "great":
+                    Console.WriteLine("I am glad to hear that your day is great!");
+                    break;
+                case "good":
+                    Console.WriteLine("That is good news!");
+                    break;
+                case "okay":
+                    Console.WriteLine("I hope your day gets better!");
+                    break;
+                case "bad":
+                    Console.WriteLine("I am sorry to hear that. Is there anything I can do to help?");
+                    break;
+                case ":(":
+                    Console.WriteLine("That sounds like a horrible day!");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        [TestMethod]
+        public void Question7()
+        {
+            string word = "Supercalifragilisticexpialidocious";
+            //--Part 1
+            foreach (char letter in word)
+            {
+                Console.WriteLine(letter);
+            }
+            //--Part 2 and Bonus 2
+            foreach (char letter in word)
+            {
+                if (letter.Equals('i') || letter.Equals('l'))
+                {
+                    Console.WriteLine(letter);
+                }
+                else
+                {
+                    Console.WriteLine("Not an i or l.");
+                }
+            }
+            //--Bonus 1
+            Console.WriteLine(word.Length);
+        }
+    }
+}
